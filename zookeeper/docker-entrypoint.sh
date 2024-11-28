e2b052c7e0bb1fe1769353618671debeb7451de1
cctpcctp <cctpcctp@noreply.gitcode.com>
2024-11-28 18:30:24 +0800

    update: 更新文件 docker-entrypoint.sh

--- zookeeper/docker-entrypoint.sh
+++ zookeeper/docker-entrypoint.sh
@@ -4,41 +4,28 @@ set -e
 
 # Allow the container to be started with `--user`
 if [[ "$1" = 'zkServer.sh' && "$(id -u)" = '0' ]]; then
-    chown -R zookeeper "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR"
-    exec gosu zookeeper "$0" "$@"
+    chown -R "$ZOO_USER" "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR" "$ZOO_CONF_DIR"
+    exec su-exec "$ZOO_USER" "$0" "$@"
 fi
 
 # Generate the config only if it doesn't exist
 if [[ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]]; then
     CONFIG="$ZOO_CONF_DIR/zoo.cfg"
-    {
-        echo "dataDir=$ZOO_DATA_DIR"
-        echo "dataLogDir=$ZOO_DATA_LOG_DIR"
 
-        echo "tickTime=$ZOO_TICK_TIME"
-        echo "initLimit=$ZOO_INIT_LIMIT"
-        echo "syncLimit=$ZOO_SYNC_LIMIT"
+    echo "clientPort=$ZOO_PORT" >> "$CONFIG"
+    echo "dataDir=$ZOO_DATA_DIR" >> "$CONFIG"
+    echo "dataLogDir=$ZOO_DATA_LOG_DIR" >> "$CONFIG"
 
-        echo "autopurge.snapRetainCount=$ZOO_AUTOPURGE_SNAPRETAINCOUNT"
-        echo "autopurge.purgeInterval=$ZOO_AUTOPURGE_PURGEINTERVAL"
-        echo "maxClientCnxns=$ZOO_MAX_CLIENT_CNXNS"
-        echo "standaloneEnabled=$ZOO_STANDALONE_ENABLED"
-        echo "admin.enableServer=$ZOO_ADMINSERVER_ENABLED"
-    } >> "$CONFIG"
-    if [[ -z $ZOO_SERVERS ]]; then
-      ZOO_SERVERS="server.1=localhost:2888:3888;2181"
-    fi
+    echo "tickTime=$ZOO_TICK_TIME" >> "$CONFIG"
+    echo "initLimit=$ZOO_INIT_LIMIT" >> "$CONFIG"
+    echo "syncLimit=$ZOO_SYNC_LIMIT" >> "$CONFIG"
+
+    echo "autopurge.snapRetainCount=$ZOO_AUTOPURGE_SNAPRETAINCOUNT" >> "$CONFIG"
+    echo "autopurge.purgeInterval=$ZOO_AUTOPURGE_PURGEINTERVAL" >> "$CONFIG"
+    echo "maxClientCnxns=$ZOO_MAX_CLIENT_CNXNS" >> "$CONFIG"
 
     for server in $ZOO_SERVERS; do
         echo "$server" >> "$CONFIG"
-    done
-
-    if [[ -n $ZOO_4LW_COMMANDS_WHITELIST ]]; then
-        echo "4lw.commands.whitelist=$ZOO_4LW_COMMANDS_WHITELIST" >> "$CONFIG"
-    fi
-
-    for cfg_extra_entry in $ZOO_CFG_EXTRA; do
-        echo "$cfg_extra_entry" >> "$CONFIG"
     done
 fi
 
